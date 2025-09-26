63c01aff73f344fc4a9130bd731e20406b52c59d
Łukasz Staniszewski <shadovekpv@gmail.com>
2025-09-26 02:46:23 +0200

    removed quotation marks

--- app/main.py
+++ app/main.py
@@ -10,12 +10,12 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: Distance | int | float) -> "Distance":
+    def __add__(self, other: Distance | int | float) -> Distance:
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
         return Distance(self.km + other)
 
-    def __iadd__(self, other: Distance | int | float) -> "Distance":
+    def __iadd__(self, other: Distance | int | float) -> Distance:
         result = self + other
         self.km = result.km
         return self

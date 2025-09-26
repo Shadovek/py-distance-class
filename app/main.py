a67aa66a3e83e22b24f1dbd03f450f33261f9127
Łukasz Staniszewski <shadovekpv@gmail.com>
2025-09-26 02:43:05 +0200

    remove unnecassary isinstance on mul and truediv

--- app/main.py
+++ app/main.py
@@ -21,12 +21,10 @@ class Distance:
         return self
 
     def __mul__(self, other: float | int) -> Distance:
-        if isinstance(other, (int, float)):
-            return Distance(self.km * other)
+        return Distance(self.km * other)
 
     def __truediv__(self, other: float | int) -> Distance:
-        if isinstance(other, (int, float)):
-            return Distance(round(self.km / other, 2))
+        return Distance(round(self.km / other, 2))
 
     def __lt__(self, other: Distance | int | float) -> bool:
         if isinstance(other, Distance):
